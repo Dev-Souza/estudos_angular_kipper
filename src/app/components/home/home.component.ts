7a00a5663436bd419cf77ca84e19d636a5cde205
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-04 18:36:03 -0300

    estados

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  meuBooleano = false;
 
+  atualizaBoleano(valor: boolean){
+    this.meuBooleano = valor;
+  }
 }
