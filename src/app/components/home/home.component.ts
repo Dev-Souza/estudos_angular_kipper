1e0dc6a123292427daf00a7ba29385b06e70007e
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-05 01:01:31 -0300

    feat: input and output

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { EnviaFormularioService } from '../../services/envia-formulario.service';
 
 @Component({
@@ -10,10 +10,16 @@ import { EnviaFormularioService } from '../../services/envia-formulario.service'
 export class HomeComponent {
   deveMostrarTitulo = true
   private enviaFormService = inject(EnviaFormularioService);
-
+  name = "Kauan Souza";
   listItems = [1,2,3,4,5]
 
+  @Input() minhaPropsDeFora!: string;
+
+  @Output() emitindoValorName = new EventEmitter<string>();
+
+  
   submit(){
+    this.emitindoValorName.emit(this.name)
     this.enviaFormService.enviaInformacao("Enviou?")
   }
 }
